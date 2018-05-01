56a7a76de3d0e8a40c628f09c3bc857d1fc26de4
Maribel Montejano <outdoorsole@users.noreply.github.com>
2018-04-30 21:25:00 -0700

    Add UI elements (textfield, btn, image view, labels)

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: - Outlets
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    // MARK: - Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchTextField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
-
-
+    
 }
-
